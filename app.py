880344d09214f1ef3dfa2e04511cf4518bac5ef6
arsen-movsesyan <arsen.movsesyan@gmail.com>
2022-01-22 23:14:39 -0800

    Modified with details about algorithms

--- app.py
+++ app.py
@@ -2,13 +2,39 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from fraction_calc_binary_tree import BinaryFractionBTree
 from fraction_calculator_math import get_fractional
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
+class Algorithms(Resource):
+
+    def get(self):
+        return [
+            {
+                "name": "Binary Tree",
+                "description": "Binary Tree used to create the entire tree of available values",
+                "github_url": "https://github.com/arsen-movsesyan/find_nearest_fraction/blob/6d9697e4b60c32ee7358b3d7215d65b0c90e74b6/fraction_calc_binary_tree.py",
+                "api_endpoint": "btree",
+                "precision_choice": [2, 3, 4, 5, 6],
+                "default_precision": 5
+            },
+            {
+                "name": "Condensed",
+                "description": "Single method with condensed algorithm",
+                "github_url": "https://github.com/arsen-movsesyan/find_nearest_fraction/blob/6d9697e4b60c32ee7358b3d7215d65b0c90e74b6/fraction_calculator_math.py",
+                "api_endpoint": "densed",
+                "precision_choice": [2, 4, 8, 16, 32, 64, 128],
+                "default_precision": 64
+        }
+        ]
+
+
 class BinaryTreeUsage(Resource):
-    precision_level = 5
+    precision = 5
     decimal_fraction = None
     result_fraction = None
     whole = None
@@ -22,13 +48,13 @@ class BinaryTreeUsage(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('decimal_fraction', type=float, required=True)
         parser.add_argument(
-            'precision_level',
+            'precision',
             type=int,
             default=5,
             choices=[2, 3, 4, 5, 6],
             help='Which value for power of two should be used for precision. Valid values are in range 2-6 inclusive')
         args = parser.parse_args(strict=True)
-        self.precision_level = args["precision_level"]
+        self.precision = args["precision"]
         self.decimal_fraction = args["decimal_fraction"]
         self._find_nearest_fraction()
         return self._get_result()
@@ -42,14 +68,14 @@ class BinaryTreeUsage(Resource):
             frac_str = str(self.decimal_fraction).split('.')[1]
             decimal_factor = 10 ** len(frac_str)
             fraction = int(frac_str)
-            ff = BinaryFractionBTree(self.precision_level)
+            ff = BinaryFractionBTree(self.precision)
             self.numerator, self.denominator = ff.find_nearest(fraction / decimal_factor)
             self.result_fraction = f"{self.whole} {self.numerator} / {self.denominator}"
 
     def _get_result(self):
         return {
             "decimal": self.decimal_fraction,
-            "precision": self.precision_level,
+            "precision": self.precision,
             "fraction": self.result_fraction,
             "whole": self.whole,
             "numerator": self.numerator,
@@ -72,14 +98,14 @@ class SingleMethodUsage(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('decimal_fraction', type=float, required=True)
         parser.add_argument(
-            'denominator',
+            'precision',
             type=int,
             default=64,
             choices=[2, 4, 8, 16, 32, 64, 128],
             help='Fraction of this value should be used. Default is 64'
         )
         args = parser.parse_args(strict=True)
-        self.precision = args['denominator']
+        self.precision = args['precision']
         self.decimal_fraction = args['decimal_fraction']
         self.whole, self.numerator, self.denominator = get_fractional( self.decimal_fraction, self.precision)
         self.result_fraction = f"{self.whole} {self.numerator} / {self.denominator}"
@@ -96,8 +122,9 @@ class SingleMethodUsage(Resource):
         }
 
 
+api.add_resource(Algorithms, '/algorithms')
 api.add_resource(BinaryTreeUsage, '/btree')
-api.add_resource(SingleMethodUsage, '/math')
+api.add_resource(SingleMethodUsage, '/densed')
 
 if __name__ == '__main__':
     app.run()
--- fraction_calculator_math.py
+++ fraction_calculator_math.py
@@ -1,14 +1,14 @@
 import math
 
 
-def get_fractional(number, max_denominator):
+def get_fractional(number, precision):
     integer = number // 1
     fraction = number % 1
     if fraction == 0:
         return [int(integer), 1, 1]
     _list = []
     denominator = 2
-    max_d = int(math.log(max_denominator, 2))
+    max_d = int(math.log(precision, 2))
     for _ in range(0, max_d):
         numerator = round(fraction * denominator)
         if numerator != 0:
